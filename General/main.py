aa49d68925b4b073568776b1bc796f9898c371a7
Anu Kuncheria <akuncheria@ses7616872.local>
2021-10-17 00:15:14 -0700

    refactored

--- General/main.py
+++ General/main.py
@@ -1,7 +1,7 @@
 import sys
 from analysis_tools import *
 
-dir = "/Users/akuncheria/Documents/GSR-2021Feb/UCBerkeley_GSR/Results_Mobiliti/2021-02-04-SFAddedFreightDemand"
+dir = "../../../../2021-02-04-SFAddedFreightDemand"
 subdir = "2021-02-03-sf_uet_000_links"
 fpath = "avg_flow_rates.tsv"
 spath = "avg_speeds.tsv"
@@ -9,7 +9,7 @@ fupath = "fuel_consumption.tsv"
 
 #Loading network
 network = Network()
-npath = "/Users/akuncheria/Documents/GSR-2021Feb/UCBerkeley_GSR/Networks_Dataset/networks_dataset_Mobiliti/Nov2019/for_drive/september2020"
+npath = "../../../../networks_dataset_Mobiliti/Nov2019/for_drive/september2020"
 nodes, links, _,  = network.load_citylinks(os.path.join(npath,'sf_nodes.csv'), os.path.join(npath,'sf_links.csv'))
 
 #Reading flow and speed files in the format along with adding the link attributes
@@ -22,11 +22,8 @@ fueldf = results.read_file(os.path.join(dir,subdir,fupath))
 print("VMT is:",vmt(flowdf))
 print("VHD is:",vhd(flowdf, speeddf))
 print("Fuel consumption is:",fuel_gallons(fueldf))
-#ADT by FC
 flowdf = ADT(flowdf)
 print(flowdf.groupby(['FUNC_CLASS'])[[ 'ADT']].sum())
-
-
 #Morning peak analysis
 cols_interest = ['link_id', '7:00', '7:15', '7:30', '7:45', '8:00', '8:15',
        '8:30', '8:45', '9:00', '9:15', '9:30', '9:45', 'ST_NAME', 'REF_IN_ID', 'NREF_IN_ID', 'FUNC_CLASS',
